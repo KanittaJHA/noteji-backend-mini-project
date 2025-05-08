3a83efdb218bdb1a8e5d6e1bcacb65c124352130
KanittaJHA <kanittaketin@gmail.com>
2025-05-09 02:19:25 +0700

    modified controllers/user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -43,7 +43,7 @@ export const createAccount = async (req, res, next) => {
             .cookie("accessToken", token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
-                sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+                sameSite: "None",
                 maxAge: 30 * 24 * 60 * 60 * 1000,
             })
             .json({
@@ -89,7 +89,7 @@ export const login = async (req, res, next) => {
             .cookie("accessToken", accessToken, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === "production",
-                sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+                sameSite: "None",
                 maxAge: 30 * 24 * 60 * 60 * 1000,
             })
             .json({
